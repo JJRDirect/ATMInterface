2a6d15ae3cbe39c5a58669ad2a8adde3ce2b2119
JJRDirect <joshrice15apple@gmail.com>
2025-10-07 09:44:23 +0100

    Added functional code to start.py

--- start.py
+++ start.py
@@ -1,4 +1,38 @@
-def test ():
-    print ("Testing")
+import pywriter as pw
 
-test()
+def ATMScreen ():
+
+    balance = 500
+    active = True
+
+    while active == True:
+        menu = input("""
+        1. Check Balance
+        2. Deposit
+        3. Withdraw
+        4. Exit
+
+        Enter your option: """)
+
+        if menu == "1":
+            print ("Your standing balance is: £", balance)
+
+        elif menu == "2":
+            deposit = int(input("How much would you like to deposit?\n Amount: "))
+            balance = balance + deposit
+            print ("You deposited: ", deposit,"\nYour new balance is: ", balance)
+
+        elif menu == "3":
+            withdraw = int(input("How much would you like to withdraw?\nAmount: "))
+            if withdraw > balance:
+                print ("\nInsufficent Funds")
+            else:
+                balance = balance - withdraw
+                print ("\nYou withdrew: ", withdraw, "\nYour new balance is: ", balance)
+
+        elif menu == "4":
+            ("\nATM Terminated")
+            active = False
+
+ATMScreen()
+
